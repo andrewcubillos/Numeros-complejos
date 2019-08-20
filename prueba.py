250703cd20c2b95a949fbb4c9a876d032a1e773e
andrewcubillos <32993313+andrewcubillos@users.noreply.github.com>
2019-08-19 23:07:07 -0500

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,18 +1,20 @@
 from numeroscomplejos import *
-import random
-def main():
-    casos=0
-    while casos<10:
-        numero1=(random.randrange(-10,-1),random.randrange(1,10))
-        numero2=(random.randrange(1,10),random.randrange(-10,-1))
-        print(suma(numero1,numero2))
-        print(suma(numero1, numero2))
-        print(producto(numero1, numero2))
-        print(division(numero1, numero2))
-        print(modulo(numero1, numero2))
-        print(conjugado(numero1, numero2))
-        print(polar(numero1,numero2))
-        print(fase(numero1,numero2))
-        casos+=1
-                                            
-main()
+import unittest
+
+class PruebasFunciones(unittest.TestCase):
+    def test(self):
+        self.assertEqual(suma((2,1),(1,3)),(3,4))
+        self.assertEqual(producto((3, -2), (1, 2)), (7, 4))
+        self.assertEqual(producto((0, 1), (0, 1)), (-1, 0))
+        self.assertEqual(resta((2, -3), (4, -1)), (-2, -2))
+        self.assertEqual(resta((3, -2), (4, -6)), (-1, 4))
+        self.assertEqual(division((-2, 1), (1, 2)), (0, 1))
+
+        self.assertEqual(modulo((3,9)),9.486832980505138)
+        self.assertEqual(conjugado((7, 10)), (7, -10))
+        self.assertEqual(polar((3,2)), (3.605551275463989,0.5880026035475675))
+        self.assertEqual(fase((2, 89)), 1.548328198209071)
+
+
+if __name__ == '__main__':
+    unittest.main()
